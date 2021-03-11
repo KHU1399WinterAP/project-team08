75d8cda29fc729dab05903acdd93c1487d232128
paria <paria_jokar@yahoo.com>
2021-03-11 14:25:00 +0330

    [MERGE]

---
+++ src/main/java/config/ValidationConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author paria
+ */
+public class ValidationConfig {
+    public static final int USERNAME_MIN_LENGTH = 3;
+	public static final int USERNAME_MAX_LENGTH = 32;
+	
+	public static final int PASSWORD_MIN_LENGTH = 3;
+	public static final int PASSWORD_MAX_LENGTH = 32;
+}
