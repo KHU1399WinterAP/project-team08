a5186620d275ba06e9edd28968e75d558e66e269
pariajokar <paria_jokar@yahoo.com>
2021-03-19 22:53:36 +0330

    done

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import gui.MainMenu;
+
+public class Main {
+    public static void main (String[]args){
+        MainMenu mainMenu= new MainMenu();
+        mainMenu.setVisible(true);
+    }
+
+
+}
