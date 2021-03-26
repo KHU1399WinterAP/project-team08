127645590c7657c7886fbe2a734610840320b84f
paria <paria_jokar@yahoo.com>
2021-03-26 22:58:50 +0430

    database password change

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -3,5 +3,5 @@ package config;
 public class DatabaseConfig {
 	public static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/colorswitch";
 	public static final String USERNAME = "root";
-	public static final String PASSWORD = "1234";
+	public static final String PASSWORD = "mysql@1234";
 }
