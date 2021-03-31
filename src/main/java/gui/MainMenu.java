e0fb2cfa0ade100a7068b8c09ae05afcb9f94cc3
BijanKHU <bijankhu@gmail.com>
2021-03-31 19:51:32 +0430

    [FIX] background image

--- src/main/java/gui/MainMenu.java
+++ src/main/java/gui/MainMenu.java
@@ -10,82 +10,62 @@ import java.io.IOException;
 import javax.swing.*;
 
 public class MainMenu extends JFrame {
-    private JButton loginButton;
-    private JButton exitButton;
-    private JButton registerButton;
-    private JPanel mainpanel;
-
-
-
-    public static void main (String[]args){
-
-   MainMenu frame= new MainMenu();
-           frame.setVisible(true);
-           frame.pack();
-
-           frame.setContentPane(frame.mainpanel);
-           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           frame.setVisible(true);
-
-           frame.pack();
-           frame.setLocationRelativeTo(null);
-
-           frame.initListeners();
-
-        }
-
-
-        private void initListeners() {
-            initRegisterButtonListeners();
-            initLoginButtonListeners();
-            initExitButtonListeners();
-        }
-        private void initRegisterButtonListeners() {
-            registerButton.addActionListener(
-                    e -> {
-                        RegisterMenu registermenu = new RegisterMenu(this);
-                        this.setVisible(false);
-                        registermenu.setVisible(true);
-                    }
-            );
-        }
-
-        private void initLoginButtonListeners() {
-            loginButton.addActionListener(
-                    e -> {
-                        LoginMenu loginmenu = new LoginMenu(this);
-                        this.setVisible(false);
-                        loginmenu.setVisible(true);
-                    }
-            );
-        }
-
-        private void initExitButtonListeners(){
-            exitButton.addActionListener(e -> dispatchEvent(new WindowEvent(this,WindowEvent.WINDOW_CLOSING)));
-
-        }
-
-    private void createUIComponents() {
-        mainpanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                try {
-                    var image = ImageIO.read(getClass().getResource("/main/resources/background.png"));
-                    g.drawImage(image, 0, 0, this);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-    }
-
-
-    class CustomJpanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics g) {
-            g.drawImage(new ImageIcon(getClass().getResource("/resources/background.png")).getImage(), 0, 0, null);
-        }
-    }
-
-
+	private JButton loginButton;
+	private JButton exitButton;
+	private JButton registerButton;
+	private JPanel mainpanel;
+	
+	public MainMenu() {
+		this.setContentPane(mainpanel);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pack();
+		setLocationRelativeTo(null);
+		
+		this.initListeners();
+	}
+	
+	private void initListeners() {
+		initRegisterButtonListeners();
+		initLoginButtonListeners();
+		initExitButtonListeners();
+	}
+	
+	private void initRegisterButtonListeners() {
+		registerButton.addActionListener(
+				e -> {
+					RegisterMenu registermenu = new RegisterMenu(this);
+					this.setVisible(false);
+					registermenu.setVisible(true);
+				}
+		);
+	}
+	
+	private void initLoginButtonListeners() {
+		loginButton.addActionListener(
+				e -> {
+					LoginMenu loginmenu = new LoginMenu(this);
+					this.setVisible(false);
+					loginmenu.setVisible(true);
+				}
+		);
+	}
+	
+	private void initExitButtonListeners() {
+		exitButton.addActionListener(e -> dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING)));
+	}
+	
+	private void createUIComponents() {
+		mainpanel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				try {
+					var image = ImageIO.read(getClass().getResource("/resources/background.png"));
+					g.drawImage(image, 0, 0, this);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+	}
 }
