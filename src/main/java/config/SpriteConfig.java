83498a0a704e1b9a4eff0982d65a1c04a0bad5c0
mobinasafaei <safaeimobina1381@gmail.com>
2021-06-01 16:23:20 +0430

    fix ball icon

--- src/main/java/config/SpriteConfig.java
+++ src/main/java/config/SpriteConfig.java
@@ -1,55 +1,31 @@
 package config;
 
-import org.jetbrains.annotations.Nullable;
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.net.URL;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.net.URL;
 
 public class SpriteConfig {
 
 
-    private static final URL BALL_URL  = SpriteConfig.class.getResource("resources/colorCircle.png");
-    //private static final URL circle_URL  = SpriteConfig.class.getResource("java/resources/colorCircle.png");
-    public  static final Icon BALL = createIcon(BALL_URL,"ball");
-    //public static final Icon circle = createIcon(circle_URL,"circle");
-    private static @Nullable
-    BufferedImage createImage(URL url, String name) {
-        try {
-            if (url != null)
-                return ImageIO.read(url);
+    private static final URL BALL_URL = SpriteConfig.class.getResource("/resources/ball.png");
 
-            printError(name);
+    public static Icon createIcon(int x, int y) {
+        BufferedImage img=null;
+        try {
+            img = ImageIO.read(SpriteConfig.BALL_URL);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        ImageIcon imageIcon = new ImageIcon(img.getScaledInstance(x,y, Image.SCALE_SMOOTH));
+        return imageIcon;
+    }}
 
-        return null;
-    }
 
 
-    private static Icon createIcon(URL url, String name ) {
-        try {
-            if (url != null) {
-                var image = ImageIO.read(url);
-                var width = image.getWidth() ;
-                var height = image.getHeight() ;
 
-                return new ImageIcon(image.getScaledInstance(width, height, Image.SCALE_SMOOTH));
-            }
 
-            printError(name);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        return null;
-    }
 
-    private static void printError(String name) {
-        System.out.printf("Cannot find %s sprite.\n", name);
-    }
-}
